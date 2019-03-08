2ac453ea25ef56041532590bb4443e3f2ebeabb5
Boldizsár Tóth <39193138+toth-boldizsar@users.noreply.github.com>
2019-03-08 20:26:47 +0100

    Maze Generation
    It actually works now

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -12,15 +12,7 @@ namespace Maze
     class Cell
     {
         public bool[] walls = { true, true, true, true};
-        public int x, y;
-
         public bool visited = false;
-        
-        public Cell(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
     }
 
     class Program
@@ -30,7 +22,7 @@ namespace Maze
         static readonly string WALL = "██";
         static readonly string PATH = "  ";
 
-        static int width = 15;
+        static int width = 25;
         static int height = 15;
 
         static Cell[,] cells = new Cell[width,height];
@@ -41,7 +33,7 @@ namespace Maze
             {
                 for (int x = 0; x < width; x++)
                 {
-                    cells[x, y] = new Cell(x, y);
+                    cells[x, y] = new Cell();
                 }
             }
             PrintGrid();
@@ -85,91 +77,73 @@ namespace Maze
             WriteLine();
         }
 
-        static int[] GetNextCell(int curr_x, int curr_y)
+        static int[] GetNextCells()
         {
-            int[] next = new int[2];
-            try
+            int[] order = { 0, 1, 2, 3 };
+            int n = 3;
+            while (n > 0)
             {
-                if (((curr_x - 1 < 0 || cells[curr_x - 1, curr_y].visited) && (curr_x + 1 > width || cells[curr_x + 1, curr_y].visited)) && ((curr_y - 1 < 0 || cells[curr_x, curr_y - 1].visited) && (curr_y + 1 > height || cells[curr_x, curr_y + 1].visited)))
-                {
-                    next[0] = 0;
-                    next[1] = 0;
-                    return next;
-                }
+                int k = random.Next(0,n);
+                int temp = order[n];
+                order[n] = order[k];
+                order[k] = temp;
+                n--;
             }
-            catch
-            {
-                next[0] = 0;
-                next[1] = 0;
-                return next;
-            }
-            int direction = random.Next(0,4);
-            switch (direction)
-            {
-                case 0:
-                    next[0] = 0;
-                    next[1] = 1;
-                    break;
-                case 1:
-                    next[0] = 1;
-                    next[1] = 0;
-                    break;
-                case 2:
-                    next[0] = 0;
-                    next[1] = -1;
-                    break;
-                case 3:
-                    next[0] = -1;
-                    next[1] = 0;
-                    break;
-            }
-            if (curr_x + next[0] < 0 || curr_x + next[0] > width-1 || curr_y - next[1] < 0 || curr_y - next[1] > height-1 || cells[curr_x + next[0], curr_y - next[1]].visited)
-                return GetNextCell(curr_x, curr_y);
-            else
-                return next;
+            return order;   
         }
 
         static void GeneratePath(int x = 0, int y = 0)
         {
-            cells[x, y].visited = true;
-            int[] nextCell = GetNextCell(x, y);
-            if (nextCell[1] == 1)
-            {
-                cells[x, y].walls[2] = false;
-                cells[x, y - nextCell[1]].walls[0] = false;
-                GeneratePath(x, y - nextCell[1]);
-            }
-            nextCell = GetNextCell(x, y);
-            if (nextCell[1] == -1)
-            {
-                cells[x,y].walls[2] = false;
-                cells[x,y - nextCell[1]].walls[0] = false;
-                GeneratePath(x, y - nextCell[1]);
-            }
-            nextCell = GetNextCell(x, y);
-            if (nextCell[0] == 1)
-            {
-                cells[x, y].walls[1] = false;
-                cells[x + nextCell[0], y].walls[3] = false;
-                GeneratePath(x + nextCell[0], y);
-            }
-            nextCell = GetNextCell(x, y);
-            if (nextCell[0] == 1)
-            {
-                cells[x,y].walls[0] = false;
-                cells[x + nextCell[0], y].walls[1] = false;
-                GeneratePath(x + nextCell[0], y);
-            }
-            if (nextCell[0] == 0 && nextCell[1] == 0)
+            if (x < 0 || x > width || y < 0 || y > height || cells[x, y].visited)
                 return;
-             
+            cells[x, y].visited = true;
             PrintGrid();
+            WriteLine(x + " " + y);
+            int[] order = GetNextCells();
+            foreach (var next in order)
+            {
+                switch(next) {
+                    case 0:
+                        if (y - 1 >= 0 && !cells[x, y-1].visited)
+                        {
+                            cells[x, y].walls[0] = false;
+                            cells[x, y - 1].walls[2] = false;
+                            GeneratePath(x, y - 1);
+                        }
+                        break;
+                    case 1:
+                        if (x + 1 < width && !cells[x + 1, y].visited)
+                        {
+                            cells[x, y].walls[1] = false;
+                            cells[x + 1, y].walls[3] = false;
+                            GeneratePath(x + 1, y);
+                        }
+                        break;
+                    case 2:
+                        if (y + 1 < height && !cells[x, y+1].visited)
+                        {
+                            cells[x, y].walls[2] = false;
+                            cells[x, y + 1].walls[0] = false;
+                            GeneratePath(x, y + 1);
+                        }
+                        break;
+                    case 3:
+                        if (x - 1 >= 0 && !cells[x-1, y].visited)
+                        {
+                            cells[x, y].walls[3] = false;
+                            cells[x - 1, y].walls[1] = false;
+                            GeneratePath(x - 1, y);
+                        }
+                        break;
+                }
+            }
         }
 
         static void Main(string[] args)
         {
             InitiateGrid();
             GeneratePath();
+            PrintGrid();
         }
     }
 }
